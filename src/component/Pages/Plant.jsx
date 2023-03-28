b5adeffcb4724f7c57c38824df7ec6fa78792da7
debanjo31 <debanjo31@gmail.com>
2023-03-28 21:23:50 +0100

    working ona plant

--- src/component/Pages/Plant.jsx
+++ src/component/Pages/Plant.jsx
@@ -1,41 +1,43 @@
-import React from 'react'
+import React, {useRef, useState} from 'react'
 import Header from '../Header'
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { useParams } from 'react-router-dom';
+import { catalogueAtom } from '../../atom/catalogue';
+import { useRecoilState } from 'recoil';
+
 
 function Plant() {
-  var settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
+  const [catalogue, setcatalogue] = useRecoilState(catalogueAtom);
+  const [sliderRef, setSliderRef] = useState(null)
+   const { id } = useParams();
+  const filteredPlant = catalogue.filter(item => item.id == id)
+  const sliderSettings = {
+    // removes default buttons
+    arrows: false,
     slidesToShow: 1,
-    slidesToScroll: 1
-  };
+    slidesToScroll: 1,
+    infinite: false,
+  }
+  
   return (
-    <div>
+    <div className='mb-8'>
         <Header />
-        <p>Parties Members</p>
-        <Slider {...settings}>
-      <div className="bg-gray-500 h-96 text-white text-center text-7xl">
-        <h3>1</h3>
+        <div className='md:hidden w-5/6 mx-auto mt-4 '>
+        <Slider ref={setSliderRef} {...sliderSettings}>
+        <div key={1}>
+          <img src={filteredPlant[0].img} className="h-96 w-full" /> 
+        </div>
+        <div key={2}>
+          <img src={filteredPlant[0].imgsub[0]} className="h-96 w-full" /> 
+        </div>
+        <div key={3}>
+          <img src={filteredPlant[0].imgsub[1]} className="h-96 w-full" /> 
+        </div> 
+      </Slider>
       </div>
-      <div className="bg-gray-500 h-96 text-white text-center text-7xl">
-        <h3>2</h3>
-      </div>
-      <div className="bg-gray-500 h-96 text-white text-center text-7xl">
-        <h3>3</h3>
-      </div>
-      <div className="bg-gray-500 h-96 text-white text-center text-7xl">
-        <h3>4</h3>
-      </div>
-      <div className="bg-gray-500 h-96 text-white text-center text-7xl">
-        <h3>5</h3>
-      </div>
-      <div className="bg-gray-500 h-96 text-white text-center text-7xl">
-        <h3>6</h3>
-      </div>
-    </Slider>
+    
     </div>
   )
 }
