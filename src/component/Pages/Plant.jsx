c3f18e888b5b0df19a939e9dd96e0696bb97da8e
debanjo31 <debanjo31@gmail.com>
2023-03-28 22:23:14 +0100

    add to cart

--- src/component/Pages/Plant.jsx
+++ src/component/Pages/Plant.jsx
@@ -21,11 +21,20 @@ function Plant() {
     slidesToScroll: 1,
     infinite: true,
   }
-  
+  const [quantity, setquantity] = useState(1);
+
+  const handleAdd = () => {
+    setquantity(quantity + 1);
+  };
+
+  const handleSubtract = () => {
+    setquantity(quantity - 1);
+  };
+
   return (
     <div className='mb-8'>
         <Header />
-        <div className='md:hidden w-5/6 mx-auto mt-4 '>
+        <div className='md:hidden w-5/6 mx-auto mt-4 z-10 '>
         <Slider ref={setSliderRef} {...sliderSettings}>
         <div key={1}>
           <img src={filteredPlant[0].img} className="h-96 w-full" /> 
@@ -37,8 +46,27 @@ function Plant() {
           <img src={filteredPlant[0].imgsub[1]} className="h-96 w-full" /> 
         </div> 
       </Slider>
-      </div>
-    
+      <p className='text-sm text-gray-600 mt-8'>{filteredPlant[0].type} </p>
+      <p className='font-bold text-lg mt-2'>&#8358;{filteredPlant[0].price}</p>
+      <form>
+        <div className='relative flex mt-2'>
+         <label className='w-2/6' >
+          Quantity:
+          </label>
+          <input
+            type="number"
+            value={quantity}
+            className="border-2 border-black p-1 w-4/6 block text-center"
+            onChange={(e) => setquantity(quantity(e.target.value))}
+          />
+          <button type='button' onClick={handleAdd} className="absolute top-0 left-40 text-lg">-</button>
+        <button type='button' onClick={handleSubtract} className="absolute top-0 right-2 text-lg">+</button>
+        </div>
+        
+        <button className='block w-full mt-4 p-2 bg-black text-white'>ADD TO CART</button>
+      </form>
+      
+      </div>    
     </div>
   )
 }
